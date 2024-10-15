c0dd058f04a3ddb290c46aa080e591e510671fdf
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-10-15 00:31:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Qual é o seu nome?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Olá, {name}, hoje é dia {currentDate:d} as {currentDate:t}!");
+Console.WriteLine($"{Environment.NewLine} Aperte qualquer botão para sair....");
+Console.ReadKey(true);
